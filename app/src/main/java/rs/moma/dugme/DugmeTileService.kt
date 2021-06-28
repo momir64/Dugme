1cd64b58f27044dccdb3af6e296287100a26dbbd
Momir Stanisic <momir.stanisic.ns@gmail.com>
2021-06-29 00:31:30 +0200

    Beta 2

--- app/src/main/java/rs/moma/dugme/DugmeTileService.kt
+++ app/src/main/java/rs/moma/dugme/DugmeTileService.kt
@@ -2,16 +2,39 @@ package rs.moma.dugme
 
 import android.content.Context
 import android.media.AudioManager
+import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
+import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
 import kotlin.concurrent.thread
 
+@RequiresApi(Build.VERSION_CODES.N)
 class DugmeTileService : TileService() {
     override fun onClick() {
         thread {
             val sharedPref = getSharedPreferences("QuickSettingsTale", Context.MODE_PRIVATE)
             if (!sharedPref.getBoolean("OnOff", false)) {
+                var duration = 1700L
+                val mediaPlayer: MediaPlayer
+                sharedPref.getString("Sound", null).let { uri ->
+                    if (uri == null)
+                        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.buzz)
+                    else {
+                        mediaPlayer = MediaPlayer()
+                        val parcelFileDescriptor = contentResolver.openFileDescriptor(uri.toUri(), "r")
+                        mediaPlayer.setDataSource(parcelFileDescriptor?.fileDescriptor)
+                        mediaPlayer.prepare()
+                        parcelFileDescriptor?.close()
+                        MediaMetadataRetriever().let {
+                            it.setDataSource(applicationContext, uri.toUri())
+                            duration = it.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)!!.toLong()
+                        }
+                    }
+                }
+                mediaPlayer.start()
                 sharedPref.edit().putBoolean("OnOff", true).apply()
                 sharedPref.edit().putLong("LastPlay", System.currentTimeMillis()).apply()
                 qsTile.state = Tile.STATE_ACTIVE
@@ -20,9 +43,8 @@ class DugmeTileService : TileService() {
                 val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
                 val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_PLAY_SOUND)
-                val mediaPlayer = MediaPlayer.create(this, R.raw.buzz)
-                mediaPlayer.start()
-                Thread.sleep(1600)
+                sharedPref.edit().putLong("Duration", duration).apply()
+                Thread.sleep((duration - 100).coerceAtLeast(0))
                 while (mediaPlayer.isPlaying)
                     Thread.sleep(5)
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, AudioManager.FLAG_PLAY_SOUND)
@@ -30,14 +52,16 @@ class DugmeTileService : TileService() {
                 qsTile.state = Tile.STATE_INACTIVE
                 qsTile.updateTile()
                 mediaPlayer.release()
-            }
+            } else
+                onStartListening()
         }
     }
 
     override fun onStartListening() {
         val sharedPref = getSharedPreferences("QuickSettingsTale", Context.MODE_PRIVATE)
         val lastPlay = sharedPref.getLong("LastPlay", -1)
-        if (lastPlay != -1L && System.currentTimeMillis() - lastPlay > 2500)
+        val duration = sharedPref.getLong("Duration", 2500)
+        if (lastPlay != -1L && System.currentTimeMillis() - lastPlay > duration)
             sharedPref.edit().putBoolean("OnOff", false).apply()
         qsTile.state = if (sharedPref.getBoolean("OnOff", false)) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
         qsTile.updateTile()
--- app/src/main/java/rs/moma/dugme/MainActivity.kt
+++ app/src/main/java/rs/moma/dugme/MainActivity.kt
@@ -1,34 +1,85 @@
 package rs.moma.dugme
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.media.AudioManager
+import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        thread {
-            val sharedPref = getSharedPreferences("QuickSettingsTale", Context.MODE_PRIVATE)
-            if (!sharedPref.getBoolean("OnOff", false)) {
-                sharedPref.edit().putBoolean("OnOff", true).apply()
-                sharedPref.edit().putLong("LastPlay", System.currentTimeMillis()).apply()
-                val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-                val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-                val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_PLAY_SOUND)
-                val mediaPlayer = MediaPlayer.create(this, R.raw.buzz)
-                mediaPlayer.start()
-                Thread.sleep(1600)
-                while (mediaPlayer.isPlaying)
-                    Thread.sleep(5)
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, AudioManager.FLAG_PLAY_SOUND)
-                sharedPref.edit().putBoolean("OnOff", false).apply()
-                mediaPlayer.release()
+        setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.dugme).setOnClickListener {
+            thread {
+                val sharedPref = getSharedPreferences("QuickSettingsTale", MODE_PRIVATE)
+                if (!sharedPref.getBoolean("OnOff", false)) {
+                    var duration = 1700L
+                    val mediaPlayer: MediaPlayer
+                    sharedPref.getString("Sound", null).let { uri ->
+                        if (uri == null)
+                            mediaPlayer = MediaPlayer.create(applicationContext, R.raw.buzz)
+                        else {
+                            mediaPlayer = MediaPlayer()
+                            val parcelFileDescriptor = contentResolver.openFileDescriptor(uri.toUri(), "r")
+                            mediaPlayer.setDataSource(parcelFileDescriptor?.fileDescriptor)
+                            mediaPlayer.prepare()
+                            parcelFileDescriptor?.close()
+                            MediaMetadataRetriever().let {
+                                it.setDataSource(applicationContext, uri.toUri())
+                                duration = it.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)!!.toLong()
+                            }
+                        }
+                    }
+                    mediaPlayer.start()
+                    sharedPref.edit().putBoolean("OnOff", true).apply()
+                    sharedPref.edit().putLong("LastPlay", System.currentTimeMillis()).apply()
+                    val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+                    val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+                    val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_PLAY_SOUND)
+                    sharedPref.edit().putLong("Duration", duration).apply()
+                    Thread.sleep((duration - 100).coerceAtLeast(0))
+                    while (mediaPlayer.isPlaying)
+                        Thread.sleep(5)
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, AudioManager.FLAG_PLAY_SOUND)
+                    sharedPref.edit().putBoolean("OnOff", false).apply()
+                    mediaPlayer.release()
+                } else {
+                    val lastPlay = sharedPref.getLong("LastPlay", -1)
+                    val duration = sharedPref.getLong("Duration", 2500)
+                    if (lastPlay != -1L && System.currentTimeMillis() - lastPlay > duration)
+                        sharedPref.edit().putBoolean("OnOff", false).apply()
+                }
             }
         }
-        finish()
+
+        findViewById<ImageView>(R.id.changeSound).setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).addCategory(Intent.CATEGORY_OPENABLE).setType("audio/*")
+            resultLauncher.launch(intent)
+        }
+
+        findViewById<ImageView>(R.id.restoreSound).setOnClickListener {
+            val sharedPref = getSharedPreferences("QuickSettingsTale", Context.MODE_PRIVATE)
+            sharedPref.edit().remove("Sound").apply()
+        }
+    }
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.data?.let {
+                applicationContext.contentResolver.takePersistableUriPermission(it, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                val sharedPref = getSharedPreferences("QuickSettingsTale", Context.MODE_PRIVATE)
+                sharedPref.edit().putString("Sound", it.toString()).apply()
+            }
+        }
     }
 }
